2581f81af12d587004e13130ff0804e9844c179f
Averybruner5924 <averybruner@yahoo.com>
2018-02-17 20:51:32 -0600

    completed what I could - so tired

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,48 @@
 
-var shows = ["heMan", "smurfs", "sonic","jem",];
+var shows = ["heman", "mario", "sonic", "shera"];
 
 function begin(event) {    
     var choice = shows[Math.floor(Math.random() * shows.length)];
     console.log(choice);
+    
 
     document.onkeyup = function guess(event) {
 
         var userGuess = event.key;
         console.log(userGuess);
-        
-    
+            
         if (userGuess === choice.charAt(0)){
-            alert("you got one!");
             document.getElementById("first").innerHTML = userGuess;
+            
         }
         if (userGuess === choice.charAt(1)){
-            alert("you got one!");
+            document.getElementById("second").innerHTML = userGuess;
         }
         if (userGuess === choice.charAt(2)){
-            alert("you got one!");
+            document.getElementById("third").innerHTML = userGuess;
+
         }
         if (userGuess === choice.charAt(3)){
-            alert("you got one!");
+            document.getElementById("fourth").innerHTML = userGuess;
+
         }
         if (userGuess === choice.charAt(4)){
-            alert("you got one!");
+            document.getElementById("fifth").innerHTML = userGuess;
+
         }
         if (userGuess === choice.charAt(5)){
-            alert("you got one!");
+            document.getElementById("sixth").innerHTML = userGuess;
         }
-        else {
-            alert("try again!")
+
+
+        var targetDiv = document.getElementById("empty-div");
+
+        targetDiv.innerHTML = "Used:";
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = userGuess;
+        targetDiv.appendChild(newDiv);
         }
+
+
     }
-}
 
