3815743d503dc72ecc22d46cb95c68a94ef7d616
Averybruner5924 <averybruner@yahoo.com>
2018-02-19 17:56:13 -0600

    Added in pseduo code of what I would have completed had I had the time and google ability to do so

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,8 @@ var shows = ["heman", "mario", "sonic", "shera"];
 function begin(event) {    
     var choice = shows[Math.floor(Math.random() * shows.length)];
     console.log(choice);
+
+// I wanted to create a function that would populate the predetermined span with the letters of the chosen word. The visibility would change when the user guessed the correct letter. This would be listed in the function below.  
     
 
     document.onkeyup = function guess(event) {
@@ -34,6 +36,9 @@ function begin(event) {
             document.getElementById("sixth").innerHTML = userGuess;
         }
 
+        // I wanted to create an if that would lower the counter after each incorrect guess. Additionally I wanted to create a counter function and a if that would end the game when the counter = 0. 
+
+        //I wanted to create a function that when the game is won would send an alert to the user and change the image of the background to one of the cartoon. 
 
         var targetDiv = document.getElementById("empty-div");
 
@@ -43,6 +48,10 @@ function begin(event) {
         targetDiv.appendChild(newDiv);
         }
 
+        //I wanted to create a variable here to store the used letters so that they would remain on the page for the user to see. 
+
 
     }
+
+    //In a perfect world I would have been able to make the click me button invisible when the game is in play and visible when the game is won. Additionally I would have figured out how to have words with spaces in them so that I could add onto the shows array. 
 
